f935a8e4b21ba7fa4db80edd92f5774c962649d3
suguru <u.sgr.22114w@gmail.com>
2017-03-27 13:21:45 +0900

    first commi

---
+++ README.md
@@ -0,0 +1,9 @@
+# ※ [STILL DEVELOPIING !!]
+
+# about
+
+squeeze net by chainer
+
+# Paper
+
+[160224 SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and <0.5MB model size](https://arxiv.org/abs/1602.07360)
---
+++ alex.py
@@ -0,0 +1,59 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import skimage.io as io
+import numpy as np
+from chainer import utils
+
+class Alex(chainer.Chain):
+
+    def __init__(self,category_num=10):
+        initializer = chainer.initializers.HeNormal()
+        super(Alex,self).__init__(
+            conv1=L.Convolution2D(3,  96, 11, stride=4),
+            conv2=L.Convolution2D(96, 256,  5, pad=2),
+            conv3=L.Convolution2D(256, 384,  3, pad=1),
+            conv4=L.Convolution2D(384, 384,  3, pad=1),
+            conv5=L.Convolution2D(384, 256,  3, pad=1),
+            fc6=L.Linear(9216, 4096),
+            fc7=L.Linear(4096, 4096),
+            fc8=L.Linear(4096, 1024)
+        )
+
+    def __call__(self,x,train=True):
+        #x = chainer.Variable(x)
+        h = F.max_pooling_2d(F.relu(
+            F.local_response_normalization(self.conv1(x))), 3, stride=2)
+        h = F.max_pooling_2d(F.relu(
+            F.local_response_normalization(self.conv2(h))), 3, stride=2)
+        h = F.relu(self.conv3(h))
+        h = F.relu(self.conv4(h))
+        h = F.max_pooling_2d(F.relu(self.conv5(h)), 3, stride=2)
+        h = F.dropout(F.relu(self.fc6(h)), train=train)
+        h = F.dropout(F.relu(self.fc7(h)), train=train)
+        h = self.fc8(h)
+        return h
+
+    def calc_loss(self,y,t):
+        loss = F.softmax_cross_entropy(y,t)
+        return loss
+
+    def accuracy_of_each_category(self,y,t):
+        y.to_cpu()
+        t.to_cpu()
+        categories = set(t.data)
+        accuracy = {}
+        for category in categories:
+            supervise_indices = np.where(t.data==category)[0]
+            predict_result_of_category = np.argmax(y.data[supervise_indices],axis=1)
+            countup = len(np.where(predict_result_of_category==category)[0])
+            accuracy[category] = countup
+        return accuracy
+
+#if __name__ == "__main__":
+    # imgpath = "/Users/suguru/Desktop/test.jpg"
+    # img = io.imread(imgpath)
+    # img = np.asarray(img).transpose(2,0,1).astype(np.float32)/255.
+    # img = img[np.newaxis]
+    # ex = model(img)
+    # print(ex)
